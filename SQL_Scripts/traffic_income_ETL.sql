718ac0f1386b77a8dbb39a380ca58a7210370f59
Lisa Barcelo <lisabarcelo@Lisas-MacBook-Pro.local>
2016-11-22 19:21:51 -0800

    adding

---
+++ SQL_Scripts/traffic_income_ETL.sql
@@ -0,0 +1,24 @@
+DROP TABLE city_traffic;
+DROP TABLE city_income;
+
+
+CREATE TABLE IF NOT EXISTS city_income_traffic AS
+SELECT distinct
+	aff_inc.region_id as metro_id,
+	aff_inc.year as aff_date,
+	aff_inc.wage as wage_usd,
+
+	traffic.region_id as metro_id,
+	traffic.year as year,
+	traffic.population as population,
+	traffic.congested_trav as congested_trav,
+	traffic.ahod_per_cons as ahod_per_cons,
+	traffic.ahod_per_cons_rank as ahod_per_cons_rank,
+	traffic.tti_val as tti_val,
+	traffic.csi_val as csi_val,
+	traffic.csi_rank as csi_rank
+
+	CASE WHEN aff_inc.region_id is not null THEN "1" END as aff_df
+FROM aff_inc
+
+	LEFT JOIN traffic on (traffic.region_id = aff_inc.region_id);
