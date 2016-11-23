e18953deb96fbf09e843f70f9429f0778db365aa
Lisa Barcelo <lisabarcelo@Lisas-MacBook-Pro.local>
2016-11-22 22:39:04 -0800

    updating

--- SQL_Scripts/traffic_income_ETL.sql
+++ SQL_Scripts/traffic_income_ETL.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS city_income_traffic AS
 SELECT distinct
-	aff_inc.region_id as metro_id,
+	aff_inc.regionid as metro_id,
 	aff_inc.year as aff_date,
 	aff_inc.wage as wage_usd,
-	traffic.region_id as metro_id,
+	traffic.region_id as metro_id2,
 	traffic.year as year,
 	traffic.population as population,
 	traffic.congested_trav as congested_trav,
@@ -14,5 +14,5 @@ SELECT distinct
 	traffic.csi_rank as csi_rank
 
 FROM aff_inc
-	LEFT JOIN traffic on (traffic.region_id = aff_inc.region_id)
-WHERE traffic.region_id is not null and aff_inc.region_id is not null;
+	LEFT JOIN traffic on (traffic.region_id = aff_inc.regionid)
+WHERE traffic.region_id is not null and aff_inc.regionid is not null;
